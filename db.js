4f9418c19fe03d99797f2eb881912694d74e7eed
pmorghen <pmorghen@gmx.ch>
2026-05-23 22:30:31 +0200

    Add files via upload

--- db.js
+++ db.js
@@ -5,18 +5,3 @@ db.version(2).stores({
     categories: "++id, name",
     questions: "++id, text, status, isTest, *categoryIds"
 });
-
-// Seed some initial categories if empty
-async function initDB() {
-    const catCount = await db.categories.count();
-    if (catCount === 0) {
-        await db.categories.bulkAdd([
-            { name: "Muskeln" },
-            { name: "Skelett" },
-            { name: "Organe" },
-            { name: "Nervensystem" }
-        ]);
-    }
-}
-
-initDB();
